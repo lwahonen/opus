416e465686cbf966a35fa3bef1c237b97e47c0ed
Lauri Ahonen <lauri.ahonen@diktamen.com>
2024-03-21 10:00:40 +0200

    Remove DRED for now

--- bin/build.cmd
+++ bin/build.cmd
@@ -1,6 +1,6 @@
 rm -rf build32
-call cmake -G "Visual Studio 17 2022" -A Win32 -S .. -B "build32" -DOPUS_DRED=ON
+call cmake -G "Visual Studio 17 2022" -A Win32 -S .. -B "build32"
 call cmake --build build32 --config Release
 rm -rf build64
-call cmake -G "Visual Studio 17 2022" -A x64 -S .. -B "build64" -DOPUS_DRED=ON
+call cmake -G "Visual Studio 17 2022" -A x64 -S .. -B "build64"
 call cmake --build build64 --config Release
