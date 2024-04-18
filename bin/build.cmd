539d7911b2c8be7b0551f587d6f5f0aea480bd52
Lauri Ahonen <lauri.ahonen@diktamen.com>
2024-04-18 11:27:47 +0300

    Build debug files too:

--- bin/build.cmd
+++ bin/build.cmd
@@ -1,6 +1,8 @@
 rm -rf build32
 call cmake -G "Visual Studio 17 2022" -A Win32 -S .. -B "build32"
 call cmake --build build32 --config Release
+call cmake --build build32 --config Debug
 rm -rf build64
 call cmake -G "Visual Studio 17 2022" -A x64 -S .. -B "build64"
 call cmake --build build64 --config Release
+call cmake --build build64 --config Debug
