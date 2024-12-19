5903bb5ec85fff82d7f848aa20c8915c577f4d4e
Lauri Ahonen <lauri.ahonen@diktamen.com>
2024-12-19 12:56:47 +0200

    Add arm64 build

--- bin/build.cmd
+++ bin/build.cmd
@@ -1,3 +1,7 @@
+rm -rf buildarm
+call cmake -G "Visual Studio 17 2022" -A ARM64 -S .. -B "buildarm" -DOPUS_STATIC_RUNTIME=ON -DOPUS_OSCE=ON 
+call cmake --build buildarm --config Release
+call cmake --build buildarm --config Debug
 rm -rf build32
 call cmake -G "Visual Studio 17 2022" -A Win32 -S .. -B "build32" -DOPUS_STATIC_RUNTIME=ON -DOPUS_OSCE=ON 
 call cmake --build build32 --config Release
