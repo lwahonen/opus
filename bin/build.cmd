dbe6f51db21bd58542d7ca6112dabba7d439bfdf
LWAhonen <lauri.ahonen@diktamen.com>
2025-10-09 13:47:30 +0300

    Separate debug

--- bin/build.cmd
+++ bin/build.cmd
@@ -1,12 +1,24 @@
 rm -rf buildarm
-call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A ARM64 -S .. -B "buildarm" -DOPUS_OSCE=ON 
+call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A ARM64 -S .. -B "buildarm" -DOPUS_OSCE=ON -DCMAKE_INSTALL_PREFIX="C:/libraries/opus/bin/releasearm"
 call cmake --build buildarm --config Release
+call cmake --install buildarm --config Release
+rm -rf buildarm
+call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A ARM64 -S .. -B "buildarm" -DOPUS_OSCE=ON -DCMAKE_INSTALL_PREFIX="C:/libraries/opus/bin/debugarm"
 call cmake --build buildarm --config Debug
+call cmake --install buildarm --config Debug
 rm -rf build32
-call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A Win32 -S .. -B "build32" -DOPUS_OSCE=ON 
+call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A Win32 -S .. -B "build32" -DOPUS_OSCE=ON -DCMAKE_INSTALL_PREFIX="C:/libraries/opus/bin/release32"
 call cmake --build build32 --config Release
+call cmake --install build32 --config Release
+rm -rf build32
+call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A Win32 -S .. -B "build32" -DOPUS_OSCE=ON -DCMAKE_INSTALL_PREFIX="C:/libraries/opus/bin/debug32"
 call cmake --build build32 --config Debug
+call cmake --install build32 --config Debug
 rm -rf build64
-call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A x64 -S .. -B "build64" -DOPUS_OSCE=ON 
+call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A x64 -S .. -B "build64" -DOPUS_OSCE=ON -DCMAKE_INSTALL_PREFIX="C:/libraries/opus/bin/release64"
 call cmake --build build64 --config Release
+call cmake --install build64 --config Release
+rm -rf build64
+call cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 17 2022" -A x64 -S .. -B "build64" -DOPUS_OSCE=ON -DCMAKE_INSTALL_PREFIX="C:/libraries/opus/bin/debug64"
 call cmake --build build64 --config Debug
+call cmake --install build64 --config Debug
